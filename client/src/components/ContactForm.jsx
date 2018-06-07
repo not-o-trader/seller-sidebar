d37b441dc1f74f3cda5f43a13509aa4250d87d9e
John Mastro <john.b.mastro@gmail.com>
2018-06-07 10:59:36 -0700

    Convert ContactForm to a class
    In preparation for giving it state.

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -58,87 +58,94 @@ const Input = styled.input`
   margin-right: 20px;
 `;
 
-const ContactForm = ({ year, make, model, price }) => {
-  const placeholder = `Enter your message here. Maximum 250 characters`;
-  let messageDefault = '';
-  if (year && make && model && price) {
-    messageDefault = `
-    Hi, I'm interested in your ${year} ${make} ${model} listed on Not-O-Trader
-    for ${price}. I'd like to get more information about this vehicle and
-    confirm its availability.  
-    `.trim().replace(/[\n ]+/g, ' ');
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <ContactFormDiv>
-      <H2>Email Seller</H2>
-      <form>
-        <Label htmlFor="message">Message</Label>
-        <TextArea
-          name="message"
-          maxLength={250}
-          rows={8}
-          placeholder={placeholder}
-          value={messageDefault}
-        >
-        </TextArea>
-        {/* <div>
-          <ContactFormInlineInput
+
+  render() {
+    const { year, make, model, price } = this.props;
+    const placeholder = 'Enter your message here. Maximum 250 characters';
+    let messageDefault = '';
+    if (year && make && model && price) {
+      messageDefault = `
+      Hi, I'm interested in your ${year} ${make} ${model} listed on Not-O-Trader
+      for ${price}. I'd like to get more information about this vehicle and
+      confirm its availability.  
+      `.trim().replace(/[\n ]+/g, ' ');
+    }
+    return (
+      <ContactFormDiv>
+        <H2>Email Seller</H2>
+        <form>
+          <Label htmlFor="message">Message</Label>
+          <TextArea
+            name="message"
+            maxLength={250}
+            rows={8}
+            placeholder={placeholder}
+            value={messageDefault}
+          >
+          </TextArea>
+          {/* <div>
+            <ContactFormInlineInput
+              name="firstName"
+              label="First Name"
+              type="text"
+              placeholder="First Name"
+            />
+            <ContactFormInlineInput
+              name="lastName"
+              label="Last Name"
+              type="text"
+              placeholder="Last Name"
+            />          
+          </div> */}
+          <ContactFormInput
             name="firstName"
             label="First Name"
             type="text"
             placeholder="First Name"
           />
-          <ContactFormInlineInput
+          <ContactFormInput
             name="lastName"
             label="Last Name"
             type="text"
             placeholder="Last Name"
-          />          
-        </div> */}
-        <ContactFormInput
-          name="firstName"
-          label="First Name"
-          type="text"
-          placeholder="First Name"
-        />
-        <ContactFormInput
-          name="lastName"
-          label="Last Name"
-          type="text"
-          placeholder="Last Name"
-        />
-        <ContactFormInput
-          name="emailAddress"
-          label="Email"
-          type="email"
-          placeholder="Email"
-        />
-        <ContactFormInput
-          name="phoneNumber"
-          label="Phone Number"
-          type="tel"
-          placeholder=""
-          optional="true"
-        />
-        <PromoDiv>
-          <DivFloat>
-            <Input type="checkbox" name="contactCheckbox"></Input>
-          </DivFloat>
-          <div>
-            Yes, I would like to receive updates, promotions, and discounts
-            from Not-O-Trader and their affiliates.
-          </div>
-        </PromoDiv>
-        <PromoDiv>
-          <p>
-            By using this service, you accept the terms of
-            our Visitor Agreement
-          </p>
-        </PromoDiv>
-        <SubmitButton type="submit">Submit</SubmitButton>
-      </form>
-    </ContactFormDiv>
-  );
-};
+          />
+          <ContactFormInput
+            name="emailAddress"
+            label="Email"
+            type="email"
+            placeholder="Email"
+          />
+          <ContactFormInput
+            name="phoneNumber"
+            label="Phone Number"
+            type="tel"
+            placeholder=""
+            optional="true"
+          />
+          <PromoDiv>
+            <DivFloat>
+              <Input type="checkbox" name="contactCheckbox"></Input>
+            </DivFloat>
+            <div>
+              Yes, I would like to receive updates, promotions, and discounts
+              from Not-O-Trader and their affiliates.
+            </div>
+          </PromoDiv>
+          <PromoDiv>
+            <p>
+              By using this service, you accept the terms of
+              our Visitor Agreement
+            </p>
+          </PromoDiv>
+          <SubmitButton type="submit">Submit</SubmitButton>
+        </form>
+      </ContactFormDiv>
+    );  
+  }
+}
 
 export default ContactForm;
