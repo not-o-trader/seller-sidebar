ce6e82d5e756b4141f04c905b03e07683657f571
John Mastro <john.b.mastro@gmail.com>
2018-06-12 16:46:18 -0700

    Use Webpack's splitChunks optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-var path = require("path");
-var SRC_DIR = path.join(__dirname, "/client/src");
-var DIST_DIR = path.join(__dirname, "/client/dist");
+const path = require("path");
+
+const SRC_DIR = path.join(__dirname, "/client/src");
+const DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
   entry: {
-    app: `${SRC_DIR}/index.jsx`
+    app: `${SRC_DIR}/index.jsx`,
+    vendor: ["styled-components"]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "initial",
+          name: "vendor",
+          test: "vendor",
+          enforce: true
+        }
+      }
+    },
+    runtimeChunk: true
   },
   output: {
     filename: "bundle.js",
